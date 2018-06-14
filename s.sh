73d678175764539c081e93c2fb61e1d6ead77a69
luyunfeng <luyunfeng@yit.com>
2018-06-14 14:53:11 +0800

    跟新配置文件

--- s.sh
+++ s.sh
@@ -1,6 +1,7 @@
 # git 拉去最新分支代码
 git pull
 git branch zzb
+source /etc/profile
 # 拷贝配置文件到指定目录
 cp /www/wwwroot/zzb.soulcoder.tech/application.yml /www/wwwroot/zzb.soulcoder.tech/soulcoder/soulcoder-web/src/main/resources/application.yml
 cp /www/wwwroot/zzb.soulcoder.tech/application-prod.yml /www/wwwroot/zzb.soulcoder.tech/soulcoder/soulcoder-web/src/main/resources/application-prod.yml
