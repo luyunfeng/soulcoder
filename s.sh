41668d16bc4d7d68ba8dddef0f5dc87cf7ea463a
luyunfeng <luyunfeng@yit.com>
2018-06-22 00:36:02 +0800

    启动文件

--- s.sh
+++ s.sh
@@ -1,13 +1,23 @@
 source /etc/profile
-# git 拉去最新分支代码
+
+echo "git 拉去最新分支代码"
 git pull
 git checkout zzb
-# 拷贝配置文件到指定目录
+
+echo "拷贝配置文件到指定目录"
 cp /www/wwwroot/zzb.soulcoder.tech/application.yml /www/wwwroot/zzb.soulcoder.tech/soulcoder/soulcoder-web/src/main/resources/application.yml
 cp /www/wwwroot/zzb.soulcoder.tech/application-prod.yml /www/wwwroot/zzb.soulcoder.tech/soulcoder/soulcoder-web/src/main/resources/application-prod.yml
+echo "============maven clean============="
+echo "============maven clean============="
+echo "============maven clean============="
 mvn clean
+echo "============mvn package============="
+echo "============mvn package============="
+echo "============mvn package============="
 mvn package -Dmaven.test.skip=true
-# 启动
+echo "============开始启动============="
+echo "============开始启动============="
+echo "============开始启动============="
 cd /www/wwwroot/zzb.soulcoder.tech/soulcoder/soulcoder-web/target/
 nohup java -jar -Xms258m -Xmx258m soulcoder.jar &
 
