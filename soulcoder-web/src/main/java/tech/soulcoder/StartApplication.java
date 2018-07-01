aff70bcbd95f0f9d05205ba2ad3f0426475897a0
luyunfeng <luyunfeng@yit.com>
2018-07-01 19:14:47 +0800

    springboot-admin集成，但是一直出错，以后排查

--- soulcoder-web/src/main/java/tech/soulcoder/StartApplication.java
+++ soulcoder-web/src/main/java/tech/soulcoder/StartApplication.java
@@ -1,14 +1,20 @@
 package tech.soulcoder;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2018年05月18日17:31:56
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@RestController
-@ComponentScan("tech.soulcoder.**")
 @EnableScheduling
 @EnableAspectJAutoProxy(exposeProxy=true)
-@MapperScan("tech.soulcoder.dao")
+@EnableAdminServer
 public class StartApplication {
     private static final Logger logger = LoggerFactory.getLogger(StartApplication.class);
 
@@ -29,9 +33,14 @@ public class StartApplication {
         SpringApplication.run(StartApplication.class, args);
     }
 
-    @GetMapping("/health")
-    public String health(){
-        logger.info("健康检查");
-        return "success";
+
+    @Profile("dev")
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
     }
 }
